0547005503ceb9c62129aa371dcd1babc6be792e
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-24 20:30:09 +0200

    update

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var tree = require('npmd-tree').tree
 var spawn = require('child_process').spawn
 var path = require('path')
 
+var nodeGyp = require.resolve('node-gyp/bin/node-gyp')
+
 var rebuild = module.exports = function (target, config, cb) {
   config.post =
     function (pkg, cb) {
       if(!pkg.gypfile) return cb(null, pkg)
       var cp =
-        spawn('node-gyp', ['rebuild'], {cwd: pkg.path})
+        spawn(nodeGyp, ['rebuild'], {cwd: pkg.path})
         .on('exit', function (code) {
           cb(code === 0 ? null : new Error('node-gyp: non-zero exit'), pkg)
         })
@@ -28,7 +30,17 @@ var rebuild = module.exports = function (target, config, cb) {
   tree(target, config, cb)
 }
 
-if(!module.parent)
-  rebuild(process.cwd(), require('npmd-config'), function (err) {
-    if(err) throw err
+if(!module.parent) {
+  var config = require('npmd-config')
+  if(config.version)
+    return console.log(require('./package').version)
+
+  rebuild(process.cwd(), config, function (err) {
+    if(err) {
+      console.error('ERROR with node-gyp')
+      throw err
+    }
   })
+
+  process.on('exit', console.error)
+}
